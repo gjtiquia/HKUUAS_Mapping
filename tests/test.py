b9744cbf6c551706fb8db6682ccca02b5fe3cb6c
GJ Tiquia <gershom.tiquia@gmail.com>
2022-03-15 13:34:43 +0800

    edited test.py

--- tests/test.py
+++ tests/test.py
@@ -11,4 +11,18 @@ __author__ = "GJTiquia"
 __email__ = "GJTiquia"
 
 
+if __name__ == "__main__":
+    # Specify image directory paths
+    # MUST END WITH "\"
+    images_path = ""
+    resized_images_path = ""
+
+    # Step 1: Image Resize
+    resize_ratio = 0.4
+    resize_all(images_path, resized_images_path, resize_ratio)
+
+    
+
+
+
 
